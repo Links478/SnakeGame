abc758457a1dfd0fba05000a5d061a2cdc047519
Huy <linkz478@hotmail.com>
2023-04-19 14:30:56 +1000

    Initial commit

---
+++ src/SnakeGame.java
@@ -0,0 +1,192 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class SnakeGame extends JPanel implements ActionListener {
+    private final int width = 300;
+    private final int height = 300;
+    private final int dotSize = 10;
+    private final int dots = 900;
+    private final int delay = 140;
+    private final int x[] = new int[dots];
+    private final int y[] = new int[dots];
+    private int dotsCount;
+    private int appleX;
+    private int appleY;
+    private boolean leftDirection = false;
+    private boolean rightDirection = true;
+    private boolean upDirection = false;
+    private boolean downDirection = false;
+    private boolean inGame = true;
+    private Timer timer;
+
+    public SnakeGame() {
+        addKeyListener(new TAdapter());
+        setBackground(Color.black);
+        setPreferredSize(new Dimension(width, height));
+        setFocusable(true);
+        initGame();
+    }
+
+    public void initGame() {
+        dotsCount = 3;
+        for (int z = 0; z < dotsCount; z++) {
+            x[z] = 50 - z * 10;
+            y[z] = 50;
+        }
+
+        locateApple();
+        timer = new Timer(delay, this);
+        timer.start();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        doDrawing(g);
+    }
+
+    public void doDrawing(Graphics g) {
+        if (inGame) {
+            g.setColor(Color.red);
+            g.fillOval(appleX, appleY, dotSize, dotSize);
+            for (int z = 0; z < dotsCount; z++) {
+                if (z == 0) {
+                    g.setColor(Color.green);
+                    g.fillRect(x[z], y[z], dotSize, dotSize);
+                } else {
+                    g.setColor(Color.yellow);
+                    g.fillRect(x[z], y[z], dotSize, dotSize);
+                }
+            }
+            Toolkit.getDefaultToolkit().sync();
+        } else {
+            gameOver(g);
+        }
+    }
+
+    public void gameOver(Graphics g) {
+        String message = "Game Over";
+        Font font = new Font("Arial", Font.BOLD, 14);
+        FontMetrics metrics = getFontMetrics(font);
+
+        g.setColor(Color.white);
+        g.setFont(font);
+        g.drawString(message, (width - metrics.stringWidth(message)) / 2, height / 2);
+    }
+
+    public void checkApple() {
+        if (x[0] == appleX && y[0] == appleY) {
+            dotsCount++;
+            locateApple();
+        }
+    }
+
+    public void move() {
+        for (int z = dotsCount; z > 0; z--) {
+            x[z] = x[(z - 1)];
+            y[z] = y[(z - 1)];
+        }
+
+        if (leftDirection) {
+            x[0] -= dotSize;
+        }
+
+        if (rightDirection) {
+            x[0] += dotSize;
+        }
+
+        if (upDirection) {
+            y[0] -= dotSize;
+        }
+
+        if (downDirection) {
+            y[0] += dotSize;
+        }
+    }
+
+    public void checkCollision() {
+        for (int z = dotsCount; z > 0; z--) {
+            if ((z > 4) && (x[0] == x[z]) && (y[0] == y[z])) {
+                inGame = false;
+            }
+        }
+
+        if (y[0] >= height) {
+            inGame = false;
+        }
+
+        if (y[0] < 0) {
+            inGame = false;
+        }
+
+        if (x[0] >= width) {
+            inGame = false;
+        }
+
+        if (x[0] < 0) {
+            inGame = false;
+        }
+
+        if (!inGame) {
+            timer.stop();
+        }
+    }
+
+    public void locateApple() {
+        int r = (int) (Math.random() * (width - dotSize));
+        appleX = r / dotSize * dotSize;
+
+        r = (int) (Math.random() * (height - dotSize));
+        appleY = r / dotSize * dotSize;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (inGame) {
+            checkApple();
+            checkCollision();
+            move();
+        }
+
+        repaint();
+    }
+
+    private class TAdapter extends KeyAdapter {
+        public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+
+            if ((key == KeyEvent.VK_LEFT) && (!rightDirection)) {
+                leftDirection = true;
+                upDirection = false;
+                downDirection = false;
+            }
+
+            if ((key == KeyEvent.VK_RIGHT) && (!leftDirection)) {
+                rightDirection = true;
+                upDirection = false;
+                downDirection = false;
+            }
+
+            if ((key == KeyEvent.VK_UP) && (!downDirection)) {
+                upDirection = true;
+                rightDirection = false;
+                leftDirection = false;
+            }
+
+            if ((key == KeyEvent.VK_DOWN) && (!upDirection)) {
+                downDirection = true;
+                rightDirection = false;
+                leftDirection = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Snake Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        SnakeGame game = new SnakeGame();
+        frame.add(game);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}

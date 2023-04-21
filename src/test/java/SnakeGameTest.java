f0a780bcb48b8b0872c629e02abb86ba604eec1a
Huy <linkz478@hotmail.com>
2023-04-21 12:59:54 +1000

    Add collision testing to JUnit.
    Signed-off-by: Huy <linkz478@hotmail.com>

--- src/test/java/SnakeGameTest.java
+++ src/test/java/SnakeGameTest.java
@@ -4,9 +4,38 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SnakeGameTest {
+
     @Test
-    public void testAddition() {
-        int sum = 1 + 2;
-        assertEquals(3, sum);
+    public void testCheckCollision() {
+        SnakeGame game = new SnakeGame();
+        game.initGame();
+        game.inGame = true;
+        game.x[0] = 0;
+        game.y[0] = 0;
+        game.leftDirection = true;
+        game.checkCollision();
+        assertFalse(game.inGame);
+
+        game.inGame = true;
+        game.x[0] = 300;
+        game.y[0] = 300;
+        game.rightDirection = true;
+        game.checkCollision();
+        assertFalse(game.inGame);
+
+        game.inGame = true;
+        game.x[0] = 150;
+        game.y[0] = 300;
+        game.upDirection = true;
+        game.checkCollision();
+        assertFalse(game.inGame);
+
+        game.inGame = true;
+        game.x[0] = 150;
+        game.y[0] = 0;
+        game.downDirection = true;
+        game.checkCollision();
+        assertFalse(game.inGame);
     }
 }
+
